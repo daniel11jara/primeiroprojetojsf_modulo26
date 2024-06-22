77119004899920d2c4d0ad30305d1ec746f1cb7d
Daniel Jara <daniel11jara@gmail.com>
2024-06-22 17:16:16 -0300

    aula 15

---
+++ src/main/java/br/com/cursojsf/PessoaBeans.java
@@ -0,0 +1,31 @@
+package br.com.cursojsf;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "pessoaBeans")
+public class PessoaBeans {
+	
+	private String nome;
+	private String sobrenome;
+	
+	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	
+
+}
