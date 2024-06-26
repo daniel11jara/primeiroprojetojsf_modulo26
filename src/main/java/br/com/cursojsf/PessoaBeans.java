0959ec3f329a5f3ed018778f56e5009f5a1ceed9
Daniel Jara <daniel11jara@gmail.com>
2024-06-25 23:12:59 -0300

    aula 34

--- src/main/java/br/com/cursojsf/PessoaBeans.java
+++ src/main/java/br/com/cursojsf/PessoaBeans.java
@@ -6,18 +6,27 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlCommandButton;
 
-@ViewScoped 
+ 
 @ManagedBean(name = "pessoaBeans")
-
+@ViewScoped
 public class PessoaBeans {
 	
 	private String nome;
 	private List<String> nomes = new ArrayList<String>();
+	private HtmlCommandButton commandButton;
+	
 	
 	public String addNome() {
 		nomes.add(nome);//adicionando o nomes na lista
-		return "";
+		
+		if (nomes.size() > 3) {//quando for maior que 3
+			commandButton.setDisabled(true);//disabilita o botão
+			return "paginanavegada?faces-redirect=true";//vai para a página -- navegação dinâmica aula 33
+		}
+		
+		return "";//e vai ficar na mesma página
 	}
 	
 	public void setNomes(List<String> nomes) {
@@ -34,6 +43,14 @@ public class PessoaBeans {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public HtmlCommandButton getCommandButton() {
+		return commandButton;
+	}
+
+	public void setCommandButton(HtmlCommandButton commandButton) {
+		this.commandButton = commandButton;
 	}
 
 	
