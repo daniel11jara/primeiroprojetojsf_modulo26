8ed997afd218a9e96480b2bb18d84be09876f4b8
Daniel Jara <daniel11jara@gmail.com>
2024-07-17 22:24:54 -0300

    aula 28.10

---
+++ src/main/java/br/com/jpautil/JPAUtil.java
@@ -0,0 +1,21 @@
+package br.com.jpautil;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {//classe responsável pela persistência
+	
+	private static EntityManagerFactory factory = null;
+	
+	static {//esse método só pode ser executado apenas uma vez
+		if (factory == null) {
+			factory = Persistence.createEntityManagerFactory("meuprimeiroprojetojsf");
+		}//método que vai garatir a persistencia dos dados
+	}
+	
+	public static EntityManager getEntityManager() {
+		return factory.createEntityManager();
+	}
+
+}
