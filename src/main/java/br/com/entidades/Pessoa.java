b4c5c36c8822e7b189b6f38ddfa57c9130d3d804
Daniel Jara <daniel11jara@gmail.com>
2024-07-30 23:36:32 -0300

    aula 29.10

--- src/main/java/br/com/entidades/Pessoa.java
+++ src/main/java/br/com/entidades/Pessoa.java
@@ -27,13 +27,39 @@ public class Pessoa implements Serializable{
 	
 	@Temporal(TemporalType.DATE)
 	private Date dataNascimento;
-	
 	private String sexo;
-	
 	private String[] frameworks;
-	
 	private boolean ativo;
+	private String login;
+	private String senha;
+	private String perfilUser;
 	
+	
+	
+	public String getPerfilUser() {
+		return perfilUser;
+	}
+
+	public void setPerfilUser(String perfilUser) {
+		this.perfilUser = perfilUser;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
 	public boolean getAtivo() {
 		return ativo;
 	}
